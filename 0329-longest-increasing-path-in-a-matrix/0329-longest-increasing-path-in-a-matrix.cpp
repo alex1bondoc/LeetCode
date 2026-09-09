709ace04f6e09b482bb849daccd06d5fb4a12152
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-09-09 16:15:26 +0300

    Time: 615 ms (5.02%), Space: 20.8 MB (58.39%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int res{};
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        auto dfs = [&](auto self, int i, int j, int val) {
+            if (i < 0 || j < 0 || i >=n || j >= m || matrix[i][j] <= val) {
+                return -1;
+            }
+            if (dp[i][j] != -1) return dp[i][j];
+            
+            int val1{matrix[i][j]};
+            dp[i][j] = 1;
+            dp[i][j] = max(dp[i][j], self(self, i + 1, j, val1) + 1);
+            dp[i][j] = max(dp[i][j], self(self, i - 1, j, val1) + 1);
+            dp[i][j] = max(dp[i][j], self(self, i, j + 1, val1) + 1);
+            dp[i][j] = max(dp[i][j], self(self, i, j - 1, val1) + 1);
+            cout << matrix[i][j] << " " << dp[i][j] << endl;
+            return dp[i][j];
+
+        };
+        for (int i{}; i < n; ++i) {
+            for (int j{}; j < m; ++j) {
+                res = max(res, dfs(dfs, i, j, -1));
+            }
+        }
+        return res;
+    }
+};
