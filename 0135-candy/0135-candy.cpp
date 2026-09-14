4f163a671e215f582e0a064a41bf75b5946bd042
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-09-14 09:46:13 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -4,19 +4,23 @@ public:
         int n = ratings.size();
         int candy = n;
         for (int i{}; i < n; ++i) {
-            if (ratings[i] == ratings[i + 1]) i++;
+            if (ratings[i] == ratings[i + 1]) i++, continue;
             int peak = 0;
             while (i < n - 1 && ratings[i] > ratings[i + 1]) {
                 peak ++;
                 candy += peak;
                 i ++;
+                if (i == n) return candy;
             }
             int valey = 0;
             while (ratings[i] < ratings[i + 1]) {
                 valey ++;
                 candy += valey;
                 i++;
+                if (i == n) return candy;
+
             }
+            candy -= min(peak, valey);
         }
 
 
