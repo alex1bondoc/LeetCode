b1881c6545a91e602e9fa26d3c09650f5f0d8684
alex1bondoc <76647815+alex1bondoc@users.noreply.github.com>
2026-09-25 12:22:04 +0300

    Time: 0 ms (100%), Space: 37.3 MB (9.44%) - LeetHub

--- 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.cpp
+++ 4056-number-of-intersecting-interval-pairs-i/4056-number-of-intersecting-interval-pairs-i.cpp
@@ -2,19 +2,27 @@ class Solution {
 public:
     long long countIntersectingIntervals(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        long long res{};
-        for (int i{}; i < n; ++i) {
-            for (int j{i + 1}; j < n; ++j) {
-                if (i == j) continue;
-                int start1{intervals[i][0]};
-                int end1{intervals[i][1]};
-                int start2{intervals[j][0]};
-                int end2{intervals[j][1]};
-                if (start1 <= end2 && start2 <= end1) {
-                    res++;
-                }
-            }
+
+        vector<int> s, e;
+
+        for (auto &x : intervals) {
+            s.push_back(x[0]);
+            e.push_back(x[1]);
         }
+
+        sort(s.begin(), s.end());
+        sort(e.begin(), e.end());
+
+        long long res = 0;
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (j < n && e[j] < s[i])
+                j++;
+
+            res += (i - j);
+        }
+
         return res;
     }
 };
